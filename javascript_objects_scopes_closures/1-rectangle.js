15ee9ee24d98f8c0b04db46accc6b2d649e80ef6
iLembe <mondli99zulu@gmail.com>
2023-10-11 15:33:19 +0200

    Check

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -5,11 +5,36 @@ class Rectangle {
     }
   }
   
-  let a = 5;
-  let b = 7;
+  // Test case: Instance width: 3 - height: -3
+  const r1 = new Rectangle(3, -3);
+  if (r1.width === 3 && r1.height === -3) {
+    console.log(r1);
+    console.log(r1.width);
+    console.log(r1.height);
+  }
   
-  const r1 = new Rectangle(a, b);
-  console.log(r1);         // Output: Rectangle { width: 5, height: 7 }
-  console.log(r1.width);   // Output: 5
-  console.log(r1.height);  // Output: 7
+  // Test case: Instance width: 3
+  const r2 = new Rectangle(3);
+  if (r2.width === 3 && r2.height === undefined) {
+    console.log(r2);
+    console.log(r2.width);
+    console.log(r2.height);
+  }
   
+  // Test case: Instance no argument
+  const r3 = new Rectangle();
+  if (r3.width === undefined && r3.height === undefined) {
+    console.log(r3);
+    console.log(r3.width);
+    console.log(r3.height);
+  }
+  
+  // Test case: Instance width: undefined, height: 3
+  const r4 = new Rectangle(undefined, 3);
+
+  if (r4.width === undefined && r3.height === 3) {
+  
+  console.log(r4);
+  console.log(r4.width);
+  console.log(r4.height);
+  }
