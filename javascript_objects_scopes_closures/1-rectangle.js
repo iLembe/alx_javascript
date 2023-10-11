14b1171c1b466ff05e85c74aa77f392056f8fa2d
iLembe <mondli99zulu@gmail.com>
2023-10-11 14:42:46 +0200

    Check Rectangle class

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -5,9 +5,10 @@ class Rectangle {
     }
   }
 
-  
+  //let a=this.w;
+  //let b=this.h;
 
-  const r1 = new Rectangle(a, b);
-  console.log(r1);         // Output: Rectangle { width: a, height: b}
-  console.log(r1.width);   // Output: a
-  console.log(r1.height);  // Output: b
+  //const r1 = new Rectangle(a, b);
+  //console.log(r1);         // Output: Rectangle { width: a, height: b}
+  //console.log(r1.width);   // Output: a
+  //console.log(r1.height);  // Output: b
