741592b8ed70716c8160d11e7f407f3a955b63d6
iLembe <mondli99zulu@gmail.com>
2023-10-11 20:46:53 +0200

    Check

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -13,10 +13,6 @@ class Rectangle {
   }
   
   // Test cases:
-  const r1 = new Rectangle(5, 7);
-  console.log(r1);         // Output: Rectangle { width: 5, height: 7 }
-  console.log(r1.width);   // Output: 5
-  console.log(r1.height);  // Output: 7
   
   const r2 = new Rectangle(0, 3);
   console.log(r2);         // Output: {}
