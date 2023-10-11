7380d629cbfa01d2fbd91fcfe71fa9104fcb81a4
iLembe <mondli99zulu@gmail.com>
2023-10-11 21:58:52 +0200

    Check

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,28 +1,23 @@
 const request = require('request');
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
-// Make a GET request to the API
-request(apiUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    const tasks = JSON.parse(body);
-    
-    // Initialize an empty object to store completed task counts per user
+    const data = JSON.parse(body);
     const completedTasks = {};
 
-    // Process the tasks and count completed tasks for each user
-    tasks.forEach((task) => {
+    data.forEach((task) => {
+      const userId = task.userId;
       if (task.completed) {
-        if (completedTasks[task.userId]) {
-          completedTasks[task.userId] += 1;
+        if (completedTasks[userId]) {
+          completedTasks[userId]++;
         } else {
-          completedTasks[task.userId] = 1;
+          completedTasks[userId] = 1;
         }
       }
     });
 
     console.log(completedTasks);
-  } else {
-    console.error('Error: Unable to fetch data from the API');
   }
 });
