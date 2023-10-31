ea4071e9c9b977990c719d3479f97de9dc679f65
iLembe <mondli99zulu@gmail.com>
2023-10-31 03:04:55 +0200

    Check again

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,15 @@
-const BaseSquare = require('./5-square'); 
-class Square extends BaseSquare {
-  constructor(size) {
-    super(size);
-  }
-
-  charPrint(c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+class Square extends require('./5-square') {
+    charPrint(c) {
+      if (c === undefined) c = 'X';
+  
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
-}
-
-module.exports = Square;
+  
+  const square = new Square(3);
+  square.charPrint('D');
+  square.double();
+  square.charPrint('O');
+  
