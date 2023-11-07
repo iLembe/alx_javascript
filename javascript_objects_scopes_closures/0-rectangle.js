efbec2461b08e9be916deceab23d70425534fb9a
iLembe <mondli99zulu@gmail.com>
2023-11-07 16:59:23 +0200

    Check

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -5,6 +5,7 @@ class Rectangle {
       // Might have to Add constructor logic here
     }*/
   }
+  module.exports = Rectangle;
   const Rectangle = require ('./0-rectangle');
   // Example of creating an instance of the Rectangle class:
   const r1 = new Rectangle();
