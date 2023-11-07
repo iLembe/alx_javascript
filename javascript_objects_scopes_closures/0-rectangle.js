b5ccbd8b5d0040a26552bcb1b0c790a3d5ae85dd
iLembe <mondli99zulu@gmail.com>
2023-11-07 17:03:43 +0200

    Fixed Rectangle

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
   module.exports = Rectangle;
   //const Rectangle = require ('./0-rectangle');
   // Example of creating an instance of the Rectangle class:
-  const r1 = new Rectangle();
+  //const r1 = new Rectangle();
   
-  console.log(r1);  // Output: Rectangle {}
-  console.log(r1.constructor);  // Output: [Function: Rectangle]
+  //console.log(r1);  // Output: Rectangle {}
+  //console.log(r1.constructor);  // Output: [Function: Rectangle]
